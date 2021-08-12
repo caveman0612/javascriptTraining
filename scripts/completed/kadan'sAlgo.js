24cc5c599d1bf834fb379f5b7c19195ad24c6ae2
caveman0612 <kyle.stinar@hotmail.com>
2021-08-12 10:44:28 -0700

    finished one working on anothergit add *

---
+++ scripts/completed/kadan'sAlgo.js
@@ -0,0 +1,23 @@
+const array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+
+console.log(kadanesAlgorithm(array));
+
+
+function kadanesAlgorithm(array) {
+    let maxSoFar = -Infinity;
+    let maxHere = -Infinity;
+    const len = array.length;
+    for (let i = 0; i < len; i++) {
+        if ((maxHere + array[i]) > array[i]) {
+            maxHere = maxHere + array[i];
+        }
+        else {
+            maxHere = array[i];
+        }
+        if (maxHere > maxSoFar) {
+            maxSoFar = maxHere;
+        }
+    }
+    return maxSoFar
+
+  }
