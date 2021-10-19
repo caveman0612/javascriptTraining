32730468b1574fcf741eec57c5c56ad0f3574b1f
caveman0612 <kyle.stinar@hotmail.com>
2021-10-18 18:09:10 -0700

    keep working bro

--- scripts/main/numberOfWaysToMakeChange/numberOfWaysToMakeChange.js
+++ scripts/main/numberOfWaysToMakeChange/numberOfWaysToMakeChange.js
@@ -1,5 +1,24 @@
 function numberOfWaysToMakeChange(n, denoms) {
-  // Write your code here.
+  denoms.sort((a, b) => b - a);
+
+  const filledArray = [];
+  for (let i = 0; i <= n; i++) {
+    let value = i;
+    for (let j = 0; i < denoms.length; j++) {
+      let coin = denoms[j];
+      if (denoms[j] <= i) {
+        let times = Math.floor(value / coin);
+        value = -times * coin;
+        filledArray[i].push([times, coin]);
+      }
+    }
+  }
+
+  return denoms;
 }
+
+let num = numberOfWaysToMakeChange(6, [1, 5]);
+
+console.log(num);
 
 module.exports = numberOfWaysToMakeChange;
--- scripts/main/numberOfWaysToMakeChange/numberOfWaysToMakeChange.test.js
+++ scripts/main/numberOfWaysToMakeChange/numberOfWaysToMakeChange.test.js
@@ -1,3 +1,9 @@
 const numberOfWaysToMakeChange = require("./numberOfWaysToMakeChange");
 
-test("should do things", () => {});
+test("should do things", () => {
+  expect(numberOfWaysToMakeChange(6, [1, 5])).toEqual([5, 1]);
+});
+
+test("should do things", () => {
+  expect(numberOfWaysToMakeChange(6, [1, 5])).toEqual([0, 1, 2, 3, 4, 5, 5]);
+});
